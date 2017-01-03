fbdb66c167114ff45b4a888877a1713793f3c4c0
Jhonatan Moncada <jhonatanmoncada@gmail.com>
2017-01-03 08:02:58 -0500

    Cambio la Pagina por defecto de Adopcion

--- apps/adopcion/views.py
+++ apps/adopcion/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Index de Adopcion")
+	return HttpResponse("Pagina Principal de Adopcion")
